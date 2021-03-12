b22802dae478e21bdd0f5f6903918f185061269e
Josep V. Monjo <jvmonjo@gmail.com>
2021-03-12 12:36:59 +0100

    ADDED: skeleton placeholder while loading

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,21 @@
 <template>
   <q-page class="bg-grey-2 q-pa-md">
     <div v-if="articlesLoading">
-      Loading...
+      <div class="row items-start">
+        <div class="col-12 col-sm-6 col-md-4 q-pa-sm" v-for="(item, index) in [1,2,3,4,5,6]" :key="index">
+          <q-card class="news-card">
+            <q-skeleton height="200px" square />
+            <q-card-section>
+              <div class="text-h6 titular">
+                <q-skeleton type="text" />
+              </div>
+              <div class="text-subtitle2">
+                <q-skeleton type="text" />
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
     </div>
     <div v-else-if="articlesError">
       {{articlesError}}
