19f4d5009b56ce9a0d19c3a3a149a39d4b26ef57
Josep V. Monjo <jvmonjo@gmail.com>
2021-03-17 19:02:40 +0100

    ADDED blog details route

---
+++ src/pages/blog/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ArticlesService } from 'src/services/articles'
+
+export default defineComponent({
+  name: 'NewsDetails',
+  setup () {
+    const articlesService = new ArticlesService()
+    const { articles, articlesLoading, articlesError } = articlesService.getAll()
+    return {
+      articles,
+      articlesLoading,
+      articlesError
+    }
+  }
+})
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
+  {
+    path: '/blog/:id',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/blog/_id.vue') }]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
