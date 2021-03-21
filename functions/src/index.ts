0d5740a5c28bdbc562d44150d3dcc97403a6357e
Josep V. Monjo <jvmonjo@gmail.com>
2021-03-21 10:15:24 +0100

    ADDED: CI/CD

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,9 @@
 import * as functions from 'firebase-functions'
-import { Request, Response } from 'express'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request: Request, response: Response) => {
+export const helloWorld: functions.HttpsFunction = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
+  response.status(200).json({ results: 'Hola món' })
 })
