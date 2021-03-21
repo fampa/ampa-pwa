59dafeb13f1df7c62a173dd33d357a36d931f6aa
Josep V. Monjo <jvmonjo@gmail.com>
2021-03-21 13:48:55 +0100

    FIXED: manually added envs

--- quasar.conf.js
+++ quasar.conf.js
@@ -10,21 +10,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { configure } = require('quasar/wrappers')
 
-const DotEnv = require('dotenv')
-const parsedEnv = DotEnv.config().parsed
-const envParser = function () {
-  // Let's stringify our variables
-  // eslint-disable-next-line no-undef
-  for (key in parsedEnv) {
-    // eslint-disable-next-line no-undef
-    if (typeof parsedEnv[key] === 'string') {
-      // eslint-disable-next-line no-undef
-      parsedEnv[key] = JSON.stringify(parsedEnv[key])
-    }
-  }
-  return parsedEnv
-}
-
 module.exports = configure(function (/* ctx */) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
@@ -72,7 +57,17 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
-      env: envParser(),
+      // env: require('dotenv').config().parsed,
+      env: {
+        GRAPHQL_URI: process.env.GRAPHQL_URI,
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+      },
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
