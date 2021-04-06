b5972bbca4e81135b5f91591cb9d45811a54e72b
Josep V. Monjo <jvmonjo@gmail.com>
2021-04-06 16:30:37 +0200

    FIXED: types from firebase admin

--- functions/src/utils/users.ts
+++ functions/src/utils/users.ts
@@ -1,13 +1,14 @@
 import * as admin from 'firebase-admin'
 import { updateClaims } from './customClaims'
 
-export const userCreated = async (user) => {
-  console.log('user created', user)
+export const userCreated = async (user: admin.auth.UserRecord) => {
+  console.log('user created', user.toJSON())
   await admin
     .auth()
     .listUsers(1)
     .then(async (listUsersResult) => {
       const isNotTheFirst = listUsersResult.users[0]
+      console.log('listUsersResult', listUsersResult)
       if (isNotTheFirst) {
         await updateClaims(user.uid)
       } else {
