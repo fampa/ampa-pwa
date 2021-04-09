0d21f04e4b615bfea462ddc2083602da64681744
monjo <jvmonjo@gmail.com>
2021-04-09 11:10:44 +0200

    ADDED: apollo reset

--- src/store/user/actions.ts
+++ src/store/user/actions.ts
@@ -3,6 +3,7 @@ import { ActionTree } from 'vuex'
 import { StateInterface } from '../index'
 import { UserStateInterface } from './state'
 import firebase from 'firebase/app'
+import { apolloClient } from 'src/boot/apollo'
 
 const actions: ActionTree<UserStateInterface, StateInterface> = {
   setUser ({ commit }, payload: Member) {
@@ -11,6 +12,7 @@ const actions: ActionTree<UserStateInterface, StateInterface> = {
 
   async logout ({ commit }) {
     await firebase.auth().signOut()
+    await apolloClient.resetStore()
     commit('setUser', null)
   }
 }
