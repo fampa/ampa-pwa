bce1f31b82593a7b3558922c9e0a2143543ac264
Josep V. Monjo <jvmonjo@gmail.com>
2021-04-12 12:07:47 +0200

    Revert "CHANGED: cache provider to session storage"
    This reverts commit 2d860f56b6a0173824be9655551f2609941ab820.

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <router-view />
 </template>
 <script lang="ts">
-import { defineComponent, provide } from 'vue'
+import { defineComponent, provide, onUnmounted } from 'vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { apolloClient } from 'src/boot/apollo'
-// import { ArticlesService } from 'src/services/articles'
+import { ArticlesService } from 'src/services/articles'
 
 export default defineComponent({
   name: 'App',
   setup () {
     provide(DefaultApolloClient, apolloClient)
 
-    // onUnmounted(async () => {
-    //   const articlesService = new ArticlesService()
-    //   await articlesService.clearCache()
-    // })
+    onUnmounted(async () => {
+      const articlesService = new ArticlesService()
+      await articlesService.clearCache()
+    })
   }
 })
 </script>
