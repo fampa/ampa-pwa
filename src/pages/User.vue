9e9c7f6a9d162578be565cedac34ddaad9d3fa4f
Josep V. Monjo <jvmonjo@gmail.com>
2021-04-15 12:23:21 +0200

    FIXED: get current user if no id

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,19 +1,25 @@
 <template>
   <q-page padding class="bg-grey-2">
     <h1 class="text-h4">{{$t('personalData')}}</h1>
-    {{ member }}
+    <div v-if="member">
+      {{ member }}
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { useRoute } from 'vue-router'
 import { MembersService } from 'src/services/members'
+import { useStore } from 'src/services/store'
+
 export default {
   name: 'PagePersonalData',
   setup () {
+    const store = useStore()
+    const currentUserId = store.state.user.user?.id.toString() || ''
     const membersService = new MembersService()
     const route = useRoute()
-    const id = route.params.id.toString()
+    const id = route.params?.id?.toString() || currentUserId
     const { member, loading, error } = membersService.getById(id)
 
     return {
