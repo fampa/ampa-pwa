398c5b513d39fd24350e1e10aeaa85b40151aba5
Josep V. Monjo <jvmonjo@gmail.com>
2021-06-04 12:25:33 +0200

    UNDO: gzip options makes build to fail

--- quasar.conf.js
+++ quasar.conf.js
@@ -69,7 +69,7 @@ module.exports = configure(function (/* ctx */) {
       // rtl: false, // https://quasar.dev/options/rtl-support
       preloadChunks: true,
       // showProgress: false,
-      gzip: true,
+      // gzip: true,
       // analyze: true,
 
       // Options below are automatically set depending on the env, set them if you want to override
