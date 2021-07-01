1fbdc97fa5c696c64f81a8065151b104699e1ad9
Josep V. Monjo <jvmonjo@gmail.com>
2021-07-01 15:25:02 +0200

    FIXED: old blog routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,11 +8,8 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
   {
-    path: '/es/blog/:id/:slug?',
-    redirect: '/:type/:id/:slug?'
-  },
-  {
     path: '/:type/:id/:slug?',
+    alias: ['/es/:type/:id/:slug?', '/ca/:type/:id/:slug?'],
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/_type/_id/_slug.vue') }]
   },
